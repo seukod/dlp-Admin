4dff99fe1ea491d527fb11aa2ed5152c2f2a13de
Benjamin Ismael Parra Avila <benjagparra@gmail.com>
2024-10-02 12:38:23 -0300

    nAAHWSHE

--- app/historial/page.js
+++ app/historial/page.js
@@ -81,61 +81,12 @@ export default function Home() {
             <TableCaption>Haga click en el lápiz para editar</TableCaption>
             <Thead>
               <Tr>
-                <Th className='esqizq'>ID</Th>
-                <Th>Título</Th>
-                <Th>Autores</Th>
-                <Th>Tags</Th>
-                <Th>Donante</Th>
-                <Th>Fecha de donación</Th>
-                <Th className='esqder'>Estado</Th>
+                <Th></Th>
+
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td>#92</Td>
-                <Td>Un golpe de suerte</Td>
-                <Td>Lucho Jara</Td>
-                <Td>comedia</Td>
-                <Td>Francisco</Td>
-                <Td>20/04/2001</Td>
-                <Td>no prestado</Td>
-              </Tr>
-              <Tr>
-                <Td>#88</Td>
-                <Td>El llamado de mi madre</Td>
-                <Td>Javier Tauler</Td>
-                <Td>romance, BL, horror, acción</Td>
-                <Td>Fosox</Td>
-                <Td>30/02/2024</Td>
-                <Td>prestado</Td>
-              </Tr>
-              <Tr>
-                <Td>#32</Td>
-                <Td>SOMOS QUINTILLIZAS</Td>
-                <Td>NEGI HARUBA</Td>
-                <Td>cine, comedia</Td>
-                <Td>Angel Leal</Td>
-                <Td>18/09/2024</Td>
-                <Td>No disponible</Td>
-              </Tr>
-              <Tr>
-                <Td>#97</Td>
-                <Td>Nana</Td>
-                <Td>Ai Yazawa</Td>
-                <Td>drama</Td>
-                <Td>Francisco</Td>
-                <Td>22/07/2024</Td>
-                <Td>no prestado</Td>
-              </Tr>
-              <Tr>
-                <Td>#95</Td>
-                <Td>Gatos</Td>
-                <Td>Juan Herrera</Td>
-                <Td>comedia</Td>
-                <Td>Franco Alun</Td>
-                <Td>22/06/2023</Td>
-                <Td>no prestado</Td>
-              </Tr>
+        
             </Tbody>
             <Tfoot>
             </Tfoot>
--- app/prestamo/page.js
+++ app/prestamo/page.js
@@ -82,60 +82,26 @@ export default function Home() {
             <Thead>
               <Tr>
                 <Th className='esqizq'>ID</Th>
-                <Th>Título</Th>
-                <Th>Autores</Th>
-                <Th>Tags</Th>
-                <Th>Donante</Th>
-                <Th>Fecha de donación</Th>
+                <Th>Libro</Th>
+                <Th>Usuario</Th>
+                <Th>Fecha Prestamo</Th>
+                <Th>Fecha devolucion</Th>
+                <Th>fecha limite</Th>
+
                 <Th className='esqder'>Estado</Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td>#92</Td>
-                <Td>Un golpe de suerte</Td>
-                <Td>Lucho Jara</Td>
-                <Td>comedia</Td>
-                <Td>Francisco</Td>
-                <Td>20/04/2001</Td>
+                <Td>#01</Td>
+                <Td>Jinx</Td>
+                <Td>Angel Desleal</Td>
+                <Td>01/11/2024</Td>
+                <Td>01/12/2024</Td>
+                <Td>14/12/2024</Td>
                 <Td>no prestado</Td>
               </Tr>
-              <Tr>
-                <Td>#88</Td>
-                <Td>El llamado de mi madre</Td>
-                <Td>Javier Tauler</Td>
-                <Td>romance, BL, horror, acción</Td>
-                <Td>Fosox</Td>
-                <Td>30/02/2024</Td>
-                <Td>prestado</Td>
-              </Tr>
-              <Tr>
-                <Td>#32</Td>
-                <Td>SOMOS QUINTILLIZAS</Td>
-                <Td>NEGI HARUBA</Td>
-                <Td>cine, comedia</Td>
-                <Td>Angel Leal</Td>
-                <Td>18/09/2024</Td>
-                <Td>No disponible</Td>
-              </Tr>
-              <Tr>
-                <Td>#97</Td>
-                <Td>Nana</Td>
-                <Td>Ai Yazawa</Td>
-                <Td>drama</Td>
-                <Td>Francisco</Td>
-                <Td>22/07/2024</Td>
-                <Td>no prestado</Td>
-              </Tr>
-              <Tr>
-                <Td>#95</Td>
-                <Td>Gatos</Td>
-                <Td>Juan Herrera</Td>
-                <Td>comedia</Td>
-                <Td>Franco Alun</Td>
-                <Td>22/06/2023</Td>
-                <Td>no prestado</Td>
-              </Tr>
+
             </Tbody>
             <Tfoot>
             </Tfoot>
