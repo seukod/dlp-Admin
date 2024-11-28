c490e20116404893911d90ecaaecafb643c30e1c
Giorgio Carlin <giorgio.carlin@alumnos.uach.cl>
2024-11-28 14:40:02 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://dlp-admin-dlp.vercel.app, https://dlp-admin-dlp.vercel.app/API/prestamo, https://dlp-admin-dlp.vercel.app/API/libro, https://dlp-prestamo.vercel.app, https://dlp-donacion.vercel.app, https://dlp-dashboard.vercel.app, http://localhost:3000/, https://localhost:3000/, http://localhost:3001/, https://localhost:3000/" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Cache-Control", value: "no-store" }
+                ]
+            }
+        ]
+    }
+}
 module.exports = nextConfig;
