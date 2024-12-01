4464b6eccac724746e40d0c2d572925a2e0642e5
Seuko <francisco.contreras.ra@gmail.com>
2024-12-01 12:49:57 -0300

    update

--- app/api/libro/route.js
+++ app/api/libro/route.js
@@ -6,6 +6,7 @@ const API_URL = 'https://dlp-api.vercel.app/libros'; // API externa
 // Manejador para GET
 export async function GET(req) {
   try {
+    // Solicitar los datos de la API externa
     const response = await fetch(API_URL);
     if (!response.ok) throw new Error('Error al obtener datos desde la API externa');
 
