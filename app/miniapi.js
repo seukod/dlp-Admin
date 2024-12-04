169d16c19a9f716e43d13f07a06cde5571fee8be
Giorgio Carlin <giorgio.carlin@alumnos.uach.cl>
2024-12-04 00:26:40 -0300

    Update miniapi.js

--- app/miniapi.js
+++ app/miniapi.js
@@ -7,42 +7,19 @@ export async function fetchAndRenderData(endpoint) {
       method: 'GET',
       headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache' }
     });
-    return response.json();
-    
+    return NextResponse.json(response, {
+      headers: {
+        'Cache-Control': 'no-store',  // Evita que la respuesta se almacene en caché
+        'Pragma': 'no-cache',         // Para navegadores más antiguos
+        'Expires': '0',               // Define que los datos ya han expirado
+      },
+    }); 
   } catch (error) {
     console.error(error);
     return null;
   }
 }
-export async function cambioAPIp(prestamos, url) {
-  try {
-    const response = await fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(prestamos), // Enviar solo el libro actualizado
-    });
 
-    if (!response.ok) {
-      const errorData = await response.text(); // Cambiar a text() para leer posibles respuestas vacías
-      console.error('Error al actualizar el prestamo:', errorData);
-      throw new Error(errorData || 'Error desconocido');
-    }
-
-    const result = await response.json().catch(() => null); // Manejo para respuestas vacías
-    if (result) {
-      console.log('Libro actualizado correctamente:', result);
-      return result;
-    } else {
-      console.warn('Respuesta vacía o no válida del servidor.');
-      return null;
-    }
-  } catch (error) {
-    console.error('Error en cambioAPI:', error);
-    throw error;
-  }
-}
 
 
 
